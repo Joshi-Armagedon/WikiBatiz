8741be0ab924b6b6f11f08d738f6d161784e14a0
よし <jsoriaf2400@alumno.ipn.mx>
2025-10-24 22:38:14 -0600

    Revision y correcion de error de String mensajeActDeportivas

--- src/WikiBatiz.java
+++ src/WikiBatiz.java
@@ -118,37 +118,7 @@ public class WikiBatiz {
         "3. Maestros titulares",
         "4. Regresar al menu inicial"};
     static String[] mensajeDeportivas = {
-        "[TITULO]
-| Actividad          | Lunes             | Martes            | Miércoles       | Jueves            | Viernes          |
-| ------------- | ----------------- | ----------------- | --------------- | ----------------- | ---------------- |
-| Actividad          | Lunes             | Martes            | Miércoles       | Jueves            | Viernes          |
-| ------------- | ----------------- | ----------------- | --------------- | ----------------- | ---------------- |
-| Actividad          | Lunes             | Martes            | Miércoles       | Jueves            | Viernes          |
-| ------------- | ----------------- | ----------------- | --------------- | ----------------- | ---------------- |
-| Actividad          | Lunes             | Martes            | Miércoles       | Jueves            | Viernes          |
-| ------------- | ----------------- | ----------------- | --------------- | ----------------- | ---------------- |
-| Actividad          | Lunes             | Martes            | Miércoles       | Jueves            | Viernes          |
-| ------------- | ----------------- | ----------------- | --------------- | ----------------- | ---------------- |
-| Actividad          | Lunes             | Martes            | Miércoles       | Jueves            | Viernes          |
-| ------------- | ----------------- | ----------------- | --------------- | ----------------- | ---------------- |
-| Actividad          | Lunes             | Martes            | Miércoles       | Jueves            | Viernes          |
-| ------------- | ----------------- | ----------------- | --------------- | ----------------- | ---------------- |
-[TITULO]
-| Actividad          | Lunes             | Martes            | Miércoles       | Jueves            | Viernes          |
-| ------------- | ----------------- | ----------------- | --------------- | ----------------- | ---------------- |
-| Actividad          | Lunes             | Martes            | Miércoles       | Jueves            | Viernes          |
-| ------------- | ----------------- | ----------------- | --------------- | ----------------- | ---------------- |
-| Actividad          | Lunes             | Martes            | Miércoles       | Jueves            | Viernes          |
-| ------------- | ----------------- | ----------------- | --------------- | ----------------- | ---------------- |
-| Actividad          | Lunes             | Martes            | Miércoles       | Jueves            | Viernes          |
-| ------------- | ----------------- | ----------------- | --------------- | ----------------- | ---------------- |
-| Actividad          | Lunes             | Martes            | Miércoles       | Jueves            | Viernes          |
-| ------------- | ----------------- | ----------------- | --------------- | ----------------- | ---------------- |
-| Actividad          | Lunes             | Martes            | Miércoles       | Jueves            | Viernes          |
-| ------------- | ----------------- | ----------------- | --------------- | ----------------- | ---------------- |
-| Actividad          | Lunes             | Martes            | Miércoles       | Jueves            | Viernes          |
-| ------------- | ----------------- | ----------------- | --------------- | ----------------- | ---------------- |
-",
+        "[TITULO]",
         "Realiza tus inscripciones en https://forms.gle./xbSYFKxs5oMV5eCG9",
         "Maestros",
         "Espera..."
